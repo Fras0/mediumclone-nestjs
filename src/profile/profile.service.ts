4dabf3b38c6dd5e74a8d4578531b0aaa876842ca
Fras0 <frasasker0@gmail.com>
2026-04-04 14:07:17 +0200

    implemented follow and unfollow functionalities

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -4,12 +4,15 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from '@app/user/user.entity';
 import { Repository } from 'typeorm';
 import { ProfileResponseInterface } from './types/profileResponse.interface';
+import { FollowEntity } from './follow.entity';
 
 @Injectable()
 export class ProfileService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(FollowEntity)
+    private readonly followRepository: Repository<FollowEntity>,
   ) {}
   async getProfile(
     currentUserId: number,
@@ -27,6 +30,73 @@ export class ProfileService {
       ...user,
       following: false,
     };
+  }
+
+  async followProfile(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      where: { username: profileUsername },
+    });
+
+    if (!user) {
+      throw new HttpException('Profile does not exist', HttpStatus.NOT_FOUND);
+    }
+
+    if (currentUserId === user.id) {
+      throw new HttpException(
+        'You cannot follow yourself',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const follow = await this.followRepository.findOne({
+      where: { followerId: currentUserId, followingId: user.id },
+    });
+
+    if (!follow) {
+      const newFollow = this.followRepository.create({
+        followerId: currentUserId,
+        followingId: user.id,
+      });
+      await this.followRepository.save(newFollow);
+    }
+
+    return {
+      ...user,
+      following: true,
+    };
+  }
+  async unfollowProfile(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      where: { username: profileUsername },
+    });
+
+    if (!user) {
+      throw new HttpException('Profile does not exist', HttpStatus.NOT_FOUND);
+    }
+
+    if (currentUserId === user.id) {
+      throw new HttpException(
+        'You cannot unfollow yourself',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    await this.followRepository.delete({
+      followerId: currentUserId,
+      followingId: user.id,
+    });
+
+    const follow = await this.followRepository.findOne({
+      where: { followerId: currentUserId, followingId: user.id },
+    });
+
+    return { ...user, following: Boolean(follow) };
   }
 
   buildProfileResponse(profile: ProfileType): ProfileResponseInterface {
